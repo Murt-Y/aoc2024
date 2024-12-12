e4f70d0fe86655fef90366fb52956a04c9e8d665
Murat <molokoid@gmail.com>
2024-12-12 08:55:48 +0300

    day 11 part 1 plus progress

--- day11.py
+++ day11.py
@@ -9,26 +9,53 @@ for x in range(len(numlist)):
     numlist[x]=int(numlist[x])
 
 
-def blink(numlist):
+def blink(num):
     newlist=[]
-    for t in numlist:
-        ts=str(t)
-        if(t==0):
-            newlist.append(1)
-        elif(len(ts)%2==0):
-            m=int(len(ts)/2)
-            m1=int(ts[:m])
-            m2=int(ts[m:])
-            newlist.append(m1)
-            newlist.append(m2)
-        else:
-            t=t*2024
-            newlist.append(t)
+    t=num
+    ts=str(t)
+    if(t==0):
+        newlist.append(1)
+    elif(len(ts)%2==0):
+        m=int(len(ts)/2)
+        m1=int(ts[:m])
+        m2=int(ts[m:])
+        newlist.append(m1)
+        newlist.append(m2)
+    else:
+        t=t*2024
+        newlist.append(t)
     return newlist
 
+cachedir=[]
+cache=[]
+
+for n in numlist:
+    cachedir.append(n)
+    cache.append(1)
+
 for x in range (25):
-    updatelist= blink(numlist)
-    numlist=updatelist.copy()
+    currentcachedir=cachedir.copy()
+    currentcache=[]
+    for n in currentcachedir:
+        currentcache.append(0)
+
+    for i in range (len(cachedir)):
+        x=cachedir[i]
+        oldoccur=cache[i]
+        newl=blink(x)
+        newnoindex=-1
+        for t in newl:
+            try:
+                newnoindex=currentcachedir.index(t)
+            except:
+                currentcachedir.append(t)
+                currentcache.append(0)
+                newnoindex=len(currentcachedir)-1
+            currentcache[newnoindex]+=oldoccur
+    cachedir=currentcachedir.copy()
+    cache=currentcache.copy()
+                      
+
 
 result1=len(numlist)
 print("Result for part 1 is ....", result1)
