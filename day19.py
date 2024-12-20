0779b840bf7adb8f8f76a46c854c3ffce0b06e2d
Murat <molokoid@gmail.com>
2024-12-20 14:30:32 +0300

    Day 19

--- day19.py
+++ day19.py
@@ -6,39 +6,96 @@ reqs=[]
 result1=0
 result2=0
 
+class pat:
+    def __init__(self, pat, dest, quant):
+        self.pat=pat
+        self.dest=dest
+        self.quant=quant
+
 for i in range(len(lines)):
     if(i==0):
         towels=lines[i].split(',')
+        towels=[item.strip() for item in towels]
 
     else:
         if(lines[i]=="\n"):
             continue
         reqs.append(lines[i].strip('\n'))
 
+# for r in reqs:
+#     activelist=[]
+#     for t in towels:
+#         length=len(t)
+#         if(r[:length]==t):
+#             activelist.append(t)
+
+
+#     searchmode=True
+#     while(searchmode==True and len(activelist)>0):
+#         templist=[]
+#         for a in activelist:
+#             for t in towels:
+#                 temp=(a+t).replace(" ", "")
+#                 length=len(temp)
+#                 if(r[:length]==temp and temp not in templist):
+#                     templist.append(temp)
+#                     if(temp==r and searchmode==True):
+#                         result1+=1
+#                         searchmode=False
+        
+#         activelist=templist.copy()
+
+# print("The result for pt 1 is: ", result1)
+
+
+
 for r in reqs:
     activelist=[]
-    for t in towels:
-        activelist.append(t)
     templist=[]
-    while(len(activelist)>0):
-        temp=activelist.pop(0)
-        length=len(temp)
-        if(r[:length]==temp):
-            templist.append(temp)
-            if(temp==r):
-                result1+=1
-            else:
-                for t in towels:
-                    activelist.append(temp+t)
-        for a in activelist:
-            for t in towels:
-                temp=(a+t).replace(" ", "")
-                length=len(temp)
-                if(r[:length]==temp):
-                    templist.append(temp)
-                    if(temp==r):
-                        result1+=1
-                    break
-        activelist=templist.copy()
+    processed=[]
+    cache=[]
+    for t in towels:
+        length=len(t)
+        if(r[:length]==t):
+            p=pat(t,[],1)
+            activelist.append(p)
 
-print("The result for pt 1 is: ", result1)
+    def find_pat(patt):
+        return next((pat for pat in activelist if pat.pat == patt), None)
+
+
+    def Move(p, c):
+        dest=p.dest
+        if len(dest)>0:
+            p.quant=0
+            for d in dest:
+                point=find_pat(d)
+                if (point==None):
+                    activelist.append(pat(d, [], c))
+                else:
+                    Move(point, c)
+        else:
+            p.quant+=c
+
+
+
+    for p in activelist:
+        for t in towels:
+            a=p.pat
+            temp=(a+t).replace(" ", "")
+            length=len(temp)
+            if(r[:length]==temp):
+                p.dest.append(temp)
+        if(p.pat!=r):
+            Move(p,p.quant)
+
+            
+
+    point=find_pat(r)
+    if (point==None):
+        pass
+    else:
+        result2+=point.quant
+
+
+print("The result for pt 2 is: ", result2)
